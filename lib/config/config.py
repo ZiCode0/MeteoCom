e6a4a8f0b307159374a798dd26fb910df291cfbb
ZiCode0 <zed.code.05@gmail.com>
2022-07-21 21:59:16 +1200

    :sparkles: update config loader :sparkles:

--- lib/config/config.py
+++ lib/config/config.py
@@ -1,11 +1,13 @@
 import os
 from collections import namedtuple
+from distutils.util import strtobool
 from pprint import pprint
 from dotenv import load_dotenv
 
 Net = namedtuple('Net', 'allowed_hosts port')
-Default = namedtuple('Default', 'export_time_format task_buffer_size report_sheet debug')
-Args = namedtuple('Args', 'mode map tasks out_file target_tasks')
+Default = namedtuple('Default', 'export_filename_time_format export_time_format task_buffer_size report_sheet debug')
+Args = namedtuple('Args', 'device_port map_path task_path')
+Routes = namedtuple('Routes', 'default status get cmd')
 
 
 class EnvConfig:
@@ -24,24 +26,48 @@ class EnvConfig:
 
         self.net = Net(allowed_hosts=str(os.environ.get("MS_HOST", default="0.0.0.0")),
                        port=int(os.environ.get("MS_PORT", default=6383)))
+
+        self.routes = Routes(default='/',
+                             status='/api/status',
+                             get='/api/get/<string:target>',
+                             cmd='/api/cmd/<string:target>')
+
         # set default env params
-        self.default = Default(export_time_format=str(os.environ.get("EXPORT_TIME_FORMAT",
-                                                                     default="%Y-%m-%d_%H:%M:%S")),
+        self.default = Default(export_filename_time_format=str(os.environ.get("EXPORT_FILENAME_TIME_FORMAT",
+                                                                              default="%Y-%m-%d")),
+                               export_time_format=str(os.environ.get("EXPORT_TIME_FORMAT",
+                                                                     default="%Y.%m.%d %H:%M:%S")),
                                task_buffer_size=int(os.environ.get("TASK_BUFFER_SIZE",
                                                                    default=5)),
                                report_sheet=str(os.environ.get("REPORT_SHEET",
                                                                default="report")),
-                               debug=bool(os.environ.get("DEBUG",
-                                                         default=False)))
+                               debug=bool(strtobool(os.environ.get("DEBUG",
+                                                                   default=False)))
+                               )
 
         # set cli args
-        self.args = Args(mode=None, map=None, tasks=None, out_file=None, target_tasks=None)
+        self.args = Args(device_port=str(os.environ.get("PORT",
+                                                        default="/dev/ttyUSB0")),
+                         map_path=str(os.environ.get("MAP_PATH",
+                                                     default="map.json")),
+                         task_path=str(os.environ.get("TASKS_PATH",
+                                                      default="tasks+server.json")),
+                         )
 
     def print_params(self):
-        params = [self.net, self.default]
+        params = [self.net, self.routes, self.default, self.args]
+        str_buffer = []
         for param in params:
+            str_buffer.append(param)
             pprint(param)
+        return str_buffer
 
     @staticmethod
     def get(value_name):
         return os.environ.get(value_name)
+
+
+class FlaskConfig(object):
+    d_mapper = None
+    d_tasker = None
+    d_workspace = None
