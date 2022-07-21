dee88f7f127b5329c4f65f3c5fcb9bcc38d66aa7
ZiCode0 <zed.code.05@gmail.com>
2022-07-21 22:09:58 +1200

    :truck: change strings :truck:

--- lib/strings.py
+++ lib/strings.py
@@ -1,7 +1,13 @@
 __project_name__ = 'MeteoCom'
+__program_version__ = 'v.1.1'
+
+
+class Console:
+    program_start = f'Program {__project_name__} {__program_version__} started.'
+
 
 cli_divider = ':'
 address_divider = ':'
 
 default_map_path = './result-map.json'
-default_tasks_path = './example-tasks.json'
+default_tasks_path = './example-tasks+server.json'
