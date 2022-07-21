4e153bf0a4bd38fe9ced14fd2119314cc3bc20fb
ZiCode0 <zed.code.05@gmail.com>
2022-07-21 21:56:55 +1200

    :sparkles: add console app interface :sparkles:

---
+++ lib/cli/app.py
@@ -0,0 +1,36 @@
+import argparse
+
+from lib import strings
+
+
+class ConsoleApp:
+    def __init__(self):
+        """
+        Initial console app function
+        """
+        self.app_name = strings.__project_name__
+        self.app_version = strings.__program_version__
+        self.author = 'ZiCode0'
+        self.contacts = '[Telegram] @MrFantomz'
+        self.args = None
+
+        # define program description
+        text = '{app_name} by {author} v.{app_version}\nContacts: {contacts}'.format(app_name=self.app_name,
+                                                                                     author=self.author,
+                                                                                     app_version=self.app_version,
+                                                                                     contacts=self.contacts)
+        # initiate the parser with a description
+        parser = argparse.ArgumentParser(description=text)
+        parser.add_argument("-v", "--version", help="show program version", action="store_true")
+        parser.add_argument("-p", "--port", help="modbus device port path", default="/dev/ttyUSB0")
+        # map_path=PWS800-map.json task_path=PWS800-tasks+server.json
+        parser.add_argument("-m", "--map_path", help="json device map file", default="map.json")
+        parser.add_argument("-t", "--task_path", help="json device tasks file", default="tasks+server.json")
+        parser.add_argument("-s", "--server", help="enable http server mode only", action="store_true")
+        # parser.add_argument("-d", "--debug", help="enable debug mode to simulate modbus device", action="store_true", default=False)
+        # parser.add_argument("-c", '--configs', help="select configs file", default="default")
+
+        self.args = parser.parse_args()
+        if self.args.version:
+            print(self.app_version)
+            quit(0)
