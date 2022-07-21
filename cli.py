d506f9ac1357b0f1f2160c59ccb99ed1f96d7f4a
ZiCode0 <zed.code.05@gmail.com>
2022-07-21 22:19:28 +1200

    :art: cli code format :art:

--- cli.py
+++ cli.py
@@ -1,10 +1,10 @@
-import sys
 import time
 
-from lib.modbus import server
+from lib.modbus import mb_server
 from lib.tasker import tasker
 from lib.mapping import mapper
 from lib.config import config
+from lib.cli import app
 
 conf = config.EnvConfig()
 
@@ -20,7 +20,7 @@ def simple_input_parser(input_args):
     kw_args = {}
     for item in input_args:
         item = item.rstrip(',')
-        if item:  # Пустой элемент, например если была одиночная запятая
+        if item:  # Empty element
             if "=" in item:
                 key, value = item.split("=", maxsplit=1)
                 kw_args[key] = value
@@ -28,23 +28,22 @@ def simple_input_parser(input_args):
 
 
 if __name__ == '__main__':
-
     # ####################### #
     # EXAMPLE CLI APP VERSION #
     # ####################### #
-
-    args, kwargs = simple_input_parser(sys.argv[1:])
+    ca = app.ConsoleApp()
+    # dict_args = simple_input_parser(sys.argv[1:])
     dm = mapper.DeviceMapper()  # init device mapper instance
-    dm.read(args[0])  # read device mapper config
-    s = server.MServer(port=target_port,
-                       rate=dm.map['info']['rate'],
-                       read_mode=dm.map['info']['protocol'],
-                       fake_connect=True)
-    t = tasker.Tasker(file_path=args[1], modbus_server=s, device_mapper=dm)
-    # print()
+    dm.read(ca.args.map_path)  # read device mapper configs
+    s = mb_server.MServer(port=target_port,
+                          rate=dm.map['info']['rate'],
+                          read_mode=dm.map['info']['protocol'],
+                          slave_address=dm.map['info']['slave_address'],
+                          fake_connect=True)  # TODO: DEBUG
+    t = tasker.Tasker(file_path=ca.args.task_path, modbus_server=s, device_mapper=dm)
     t.start()
-    print('started')
 
-    time.sleep(40)
-    print(f'manual: {s.read_data(register=3)}')
-    time.sleep(500)
+    print('started')
+    time.sleep(30)
+    t.stop()
+    print('stopped')
