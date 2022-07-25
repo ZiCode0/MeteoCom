f581639b9166b47ccadc4df0441819ce45d74e6e
ZiCode0 <zed.code.05@gmail.com>
2022-07-25 22:05:22 +1200

    :white_check_mark: example connection loop :white_check_mark:

--- test/connection_example.py
+++ test/connection_example.py
@@ -1,4 +1,10 @@
 from lib.modbus.mb_server import MServer
 
-ms = MServer(port='/dev/ttyUSB0', rate=57600, read_mode='rtu')
-data = ms.read_data(register=1)
+ms = MServer(port='/dev/ttyUSB0', rate=19200, read_mode='rtu', slave_address=48, async_mode=True)
+
+try:
+    res = ms.read_data(6, read_type='default')
+except Exception as ex:
+    res = ex
+
+print(f'{res}')
